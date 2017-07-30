41cea4e0ce9f6fd982cc85fa71bf1a6684eb82c4
Your Name <you@example.com>
2017-07-30 14:08:04 -0700

    added logging

--- server.py
+++ server.py
@@ -19,12 +19,18 @@ class MyServer(socketserver.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
         print(self.data)
-        # just send back the same data, but upper-cased
-        processed = self.process_message(self.data.decode(encoding="UTF-8"))
+        # just send back the same data, but upper-
+        raw_request = self.data.decode(encoding='UTF-8')
+        # log the raw message
+        self.log_message(raw_request)
+        # turn into json object
+        request = json.loads(raw_request)
+        # process the message
+        processed = self.process_message(request)
+        # send response
         self.request.sendall(bytes(processed, "utf-8"))
 
-    def process_message(self, data):
-        message = json.loads(data)
+    def process_message(self, message):
         param = message['parameter']
         body = message['data']
         if param == 'upper':
@@ -33,6 +39,12 @@ class MyServer(socketserver.BaseRequestHandler):
             return body.lower()
         else:
             return body
+
+    def log_message(self, message):
+        with open('logfile.txt', 'a') as f:
+            f.write(message + '\n')
+        return
+
 
 def server_main():
     HOST, PORT = "localhost", 9999
